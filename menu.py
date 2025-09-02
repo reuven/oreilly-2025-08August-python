942bd593495b26502079f5affce3002a2632b77c
Reuven M. Lerner <reuven@lerner.co.il>
2025-09-03 01:57:33 +0800

    gitautosync update 66

--- menu.py
+++ menu.py
@@ -1,6 +1,9 @@
+print(__name__)
+
 def menu(options):
     while True:
         choice = input(f'Choose: {options}').strip()
     
         if choice in options:    # is the user's entered string one of the items in options?
             return choice
+
